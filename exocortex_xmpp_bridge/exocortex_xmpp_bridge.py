7a0d1735ee4daa708546ac7253b949f061d3793c
The Doctor <drwho@virtadpt.net>
2015-09-30 22:23:00 -0700

    Updated the default config file to take into account changes in the code.
    Fixed the comments in the header.
    
    Bumped to v1.1.
    
    Moved the "from" imports out of the "import"s.
    
    Replaced optparse with argparse.  It simplified the code quite a bit.  Got
    rid of the get_options() function at the same time.
    
    Knocked two TODOs off the list.
    
    Moved global variables and handles up to the global variable bit to make the
    code a bit easier to read weeks or months later.
    
    Reworked logging slightly to be more in line with how the logging module is
    meant to be used.
    
    Reformatted the core code to be one Python indent (four spaces) closer to the
    left.  It's more my style.
    
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- exocortex_xmpp_bridge/exocortex_xmpp_bridge.py
+++ exocortex_xmpp_bridge/exocortex_xmpp_bridge.py
@@ -4,12 +4,27 @@
 
 # exocortex_xmpp_bridge.py - A microservice that logs into an XMPP server with
 #   credentials from a configuration file, builds message queues for Huginn
-#   Huginn agent networks in the config file, and listens for messages sent 
-#   from the designated owner.  Each message must contain the name of the
-#   agent the command is for.  The microservice pushes messages for the agents
-#   into matching message queues retrieved later via a REST interface.
-#   Messages to unmatched agents are dropped and an error is sent back to the
-#   sender if the address of origin is this agent's designated owner.
+#   agent networks in the config file, and listens for messages sent from the
+#   designated owner.  Each message must contain the name of the agent the
+#   command is for.  The microservice pushes messages for the agents into
+#   matching message queues retrieved later via a REST interface.  Messages to
+#   unmatched agents are dropped and an error is sent back to the sender if
+#   the address of origin is this agent's designated owner.
+#
+#   This is part of the Exocortex Halo project
+#   (https://github.com/virtadpt/exocortex-halo/).
+
+# v1.1 - Switched out OptionParser in favor of argparse.
+#      - Refactored the code to make a little more sense.  argparse helped a
+#        lot with that.
+#      - Reworked logging a little.
+#      - Declared variables at the tops of everything for maintainability.
+#        Having to deal with a phantom variable that pops out of nowhere is
+#        kind of annoying.
+#      - Setting SleekXMPP to "block=True" makes it easier to kill from the
+#        command line.
+#      - Set the default loglevel to INFO.
+# v1.0 - Initial release.
 
 # TODO:
 # - Write a signal handler that makes the agent reload its configuration file
@@ -17,10 +32,6 @@
 # - Consider adding a SQLite database to serialize the message queues to in
 #   the event the microservice ABENDs or gets shut down.  Don't forget a
 #   scream-and-die error message to not leave the user hanging.
-# - Handle messages going into spurious message queues.  One misspelling could
-#   wreak havoc.
-# - Clean up main() by breaking some stuff (like argument parsing) out into
-#   separate functions.  Maybe config file parsing, too.
 
 # By: The Doctor <drwho at virtadpt dot net>
 #     0x807B17C1 / 7960 1CDC 85C9 0B63 8D9F  DD89 3BD8 FF2B 807B 17C1
@@ -29,10 +40,11 @@
 
 from BaseHTTPServer import HTTPServer
 from BaseHTTPServer import BaseHTTPRequestHandler
+
+import argparse
 import ConfigParser
 import json
 import logging
-from optparse import OptionParser
 import os
 import sleekxmpp
 import sys
@@ -42,12 +54,22 @@ import sys
 # lists which implement the message queues.
 message_queue = {}
 
+# Handle for the command line argument parser.
+args = ""
+
+# Path to and name of the configuration file.
+config_file = ""
+
+# Logging for the XMPP bridge.  Defaults to INFO.
+loglevel = ""
+
 # XMPPBot: XMPP bot class built from SleekXMPP that connects to an XMPP server
 #   specified in the configuration file with matching credentials.
 class XMPPBot(sleekxmpp.ClientXMPP):
 
     # Initializer method for the XMPPBot class.
     def __init__(self, jid, password):
+        logger.debug("Now initializing an XMPPBot.")
         super(XMPPBot, self).__init__(jid, password)
 
         # Set up an event handler for when the XMPPBot starts up.
@@ -67,8 +89,7 @@ class XMPPBot(sleekxmpp.ClientXMPP):
         # Note: This can time out under bad conditions.  Consider putting it
         # inside a try/except to retry or error out.
         self.get_roster()
-
-        logging.debug("I've successfully connected to the XMPP server.")
+        logger.debug("I've successfully connected to the XMPP server.")
 
     # Method that fires as an event handler when an XMPP message is received
     # from someone
@@ -76,10 +97,9 @@ class XMPPBot(sleekxmpp.ClientXMPP):
         # Test to see if the message came from the agent's owner.  If it did
         # not, drop the message and return.
         message_from = str(message['from']).split('/')[0]
-
         if message_from != owner:
-            logging.warn("Received a message from someone that isn't authorized.")
-            logging.warn("Message was sent from JID " + str(message['from']) + ".")
+            logger.warn("Received a message from someone that isn't authorized.")
+            logger.warn("Message was sent from JID " + str(message['from']) + ".")
             return
 
         # Potential message types: normal, chat, error, headline, groupchat
@@ -101,22 +121,23 @@ class XMPPBot(sleekxmpp.ClientXMPP):
             command = command.strip()
             command = command.strip('.')
             command = command.lower()
-            logging.debug(command)
+            logger.debug(command)
 
             # Push the command into the agent's message queue.
             message_queue[agent].append(command)
-            logging.debug("The message queue for " + agent + " now contains: " + str(message_queue[agent]))
+            logger.debug("The message queue for " + agent + " now contains: " + str(message_queue[agent]))
 
 # RESTRequestHandler: Subclass that implements a REST API service.  The main
 #   rails are the names of agent networks that will poll message queues for
 #   commands.  Each time they poll, they get a JSON dump of all of the
 #   commands waiting for them.
 class RESTRequestHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         # If someone requests /, return the current internal configuration of
         # this microservice to be helpful.
         if self.path == '/':
-            logging.debug("User requested /.  Returning online help.")
+            logger.debug("User requested /.  Returning online help.")
             self.send_response(200)
             self.send_header("Content-type:", "application/json")
             self.wfile.write('\n')
@@ -127,7 +148,7 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
         # monitoring this microservice.  If not, return a 404.
         agent = self.path.strip('/')
         if agent not in message_queue.keys():
-            logging.debug("Message queue for agent " + agent + " not found.")
+            logger.debug("Message queue for agent " + agent + " not found.")
             self.send_response(404)
             self.send_header("Content-type:", "application/json")
             self.wfile.write('\n')
@@ -136,7 +157,7 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
 
         # If the message queue is empty, return an error JSON document.
         if not len(message_queue[agent]):
-            logging.debug("Message queue for agent " + agent + " is empty.")
+            logger.debug("Message queue for agent " + agent + " is empty.")
             self.send_response(200)
             self.send_header("Content-type:", "application/json")
             self.wfile.write('\n')
@@ -149,7 +170,7 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
         # Assemble a JSON document of the earliest pending command.  Then send
         # the JSON document to the agent.  Multiple hits will be required to
         # empty the queue.
-        logging.debug("Returning earliest command from message queue " + agent + ": " + command)
+        logger.debug("Returning earliest command from message queue " + agent + ": " + command)
         self.send_response(200)
         self.send_header("Content-type:", "application/json")
         self.wfile.write('\n')
@@ -175,108 +196,83 @@ def process_loglevel(loglevel):
     if loglevel == "notset":
         return 0
 
-# get_options: Parses command line arguments passed to this daemon.
-def get_options():
-
-    # Instantiate a command line options parser.
-    optionparser = OptionParser()
-
-    # Define command line switches for the bot, starting with being able to
-    # specify an arbitrary configuration file for a particular bot.
-    optionparser.add_option("-c", "--conf", dest="configfile", action="store",
-        type="string", help="Specify a config file for this agent.  Defaults to exocortex_xmpp_bridge.conf.")
-
-    # Add a command line option that lets you override the config file's
-    # loglevel.  This is for kicking a bot into debug mode without having to
-    # edit the config file.
-    optionparser.add_option("-l", "--loglevel", dest="loglevel", action="store",
-        help="Specify the default logging level of the bot.  Valid options are CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET.  Defaults to INFO.")
-
-    # Parse the command line args.
-    (options, args) = optionparser.parse_args()
-
-    # Return the parsed options.
-    return options
-
 # Core code...
-if __name__ == '__main__':
-    # If we're running in a Python environment earlier than v3.0, set the
-    # default text encoding to UTF-8 because XMPP requires it.
-    if sys.version_info < (3, 0):
-        reload(sys)
-        sys.setdefaultencoding('utf-8')
+# If we're running in a Python environment earlier than v3.0, set the
+# default text encoding to UTF-8 because XMPP requires it.
+if sys.version_info < (3, 0):
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
-    # Parse the command line args.
-    options = get_options()
+# Set up the command line argument parser.
+argparser = argparse.ArgumentParser(description="A microservice that logs into an XMPP server with credentials from a configuration file, builds message queues for the Huginn agent networks listed in the config file, and listens for messages sent from the microservice's designated owner.")
 
-    # Read the configuration file.  There is a command line argument for
-    # specifying a configuration file, but it defaults to taking the name
-    # of the bot and appending '.conf' to it.  Then load it into a config file
-    # parser object.
-    config = ConfigParser.ConfigParser()
-    if options.configfile:
-        # Try to open the config file passed as a command line argument.
-        if not os.path.exists(options.configfile):
-            logging.error("Unable to find configuration file " + str(options.configfile) + ".")
-            sys.exit(1)
-        config.read(options.configfile)
-    else:
-        # Try to open the default config file.
-        if not os.path.exists("exocortex_xmpp_bridge.conf"):
-            logging.error("Unable to find configuration file exocortex_xmpp_bridge.conf.")
-            sys.exit(1)
-        config.read("exocortex_xmpp_bridge.conf")
-    conf = config.sections()[0]
+# Set the default config file and the command line option to specify a new one.
+argparser.add_argument('--config', action='store',
+    default='exocortex_xmpp_bridge.conf' )
 
-    # Get configuration options from the configuration file.
-    owner = config.get(conf, "owner")
-    username = config.get(conf, "username")
-    password = config.get(conf, "password")
-    agents = config.get(conf, "agents")
+# Loglevels: critical, error, warning, info, debug, notset.
+argparser.add_argument('--loglevel', action='store', default='logging.INFO',
+    help='Valid log levels: critical, error, warning, info, debug, notset.  Defaults to INFO.')
 
-    # Get the names of the agents to set up queues for from the config file.
-    for i in agents.split(','):
-        message_queue[i] = []
+# Parse the command line args.
+args = argparser.parse_args()
+if args.config:
+    config_file = args.config
 
-    # Figure out how to configure the logger.  Start by reading from the config
-    # file.
-    config_log = config.get(conf, "loglevel").lower()
-    if config_log:
-        loglevel = process_loglevel(config_log)
+# Read the configuration file.  Then load it into a config file parser object.
+config = ConfigParser.ConfigParser()
+if not os.path.exists(config_file):
+    logging.error("Unable to find or open configuration file " +
+        config_file + ".")
+    sys.exit(1)
+config.read(config_file)
 
-    # Then try the command line.
-    if options.loglevel:
-        loglevel = process_loglevel(options.loglevel.lower())
+# Get configuration options from the configuration file.
+owner = config.get("DEFAULT", "owner")
+username = config.get("DEFAULT", "username")
+password = config.get("DEFAULT", "password")
+agents = config.get("DEFAULT", "agents")
 
-    # Default to WARNING.
-    if not options.loglevel and not loglevel:
-        loglevel = logging.WARNING
+# Get the names of the agents to set up queues for from the config file.
+for i in agents.split(','):
+    message_queue[i] = []
 
-    # Configure the logger with the base loglevel.
-    logging.basicConfig(level=loglevel, format="%(levelname)s: %(message)s")
+# Figure out how to configure the logger.  Start by reading from the config
+# file.
+config_log = config.get("DEFAULT", "loglevel").lower()
+if config_log:
+    loglevel = process_loglevel(config_log)
 
-    # Instantiate a copy of XMPPBot.
-    xmppbot = XMPPBot(username, password)
+# Then try the command line.
+if args.loglevel:
+    loglevel = process_loglevel(args.loglevel.lower())
 
-    # Enable the Service Discovery plugin.
-    xmppbot.register_plugin("xep_0030")
+# Configure the logger with the base loglevel.
+logging.basicConfig(level=loglevel, format="%(levelname)s: %(message)s")
+logger = logging.getLogger(__name__)
 
-    # Enable the Ping plugin.
-    xmppbot.register_plugin("xep_0199")
+# Instantiate a copy of XMPPBot.
+xmppbot = XMPPBot(username, password)
 
-    # Connect to the XMPP server and commence operation.  SleekXMPP's state
-    # engine will run inside its own thread because we have other concerns
-    # also.
-    if xmppbot.connect():
-        xmppbot.process(block=False)
-    else:
-        logging.fatal("Uh-oh - unable to connect to JID " + username + ".")
-        sys.exit(1)
+# Enable the Service Discovery plugin.
+xmppbot.register_plugin("xep_0030")
 
-    # Allocate and start the Simple HTTP Server instance.
-    api_server = HTTPServer(("localhost", 8003), RESTRequestHandler)
-    logging.debug("REST API server now listening on localhost, port 8003/tcp.")
-    api_server.serve_forever()
+# Enable the Ping plugin.
+xmppbot.register_plugin("xep_0199")
+
+# Connect to the XMPP server and commence operation.  SleekXMPP's state engine
+# will run inside its own thread because we have other concerns also.
+# also.
+if xmppbot.connect():
+    xmppbot.process(block=True)
+else:
+    logger.fatal("Uh-oh - unable to connect to JID " + username + ".")
+    sys.exit(1)
+
+# Allocate and start the Simple HTTP Server instance.
+api_server = HTTPServer(("localhost", 8003), RESTRequestHandler)
+logger.debug("REST API server now listening on localhost, port 8003/tcp.")
+api_server.serve_forever()
 
 # Fin.
 
