b0a7f2d87b23a9e862afe966ab691c52c9b213e5
The Doctor <drwho@virtadpt.net>
2016-01-01 15:21:27 -0800

    Reworked the header comments a little.
    Updated the TODO list.
    
    Pulled in the floating point division module.
    
    Moved most of the constants inside the DixieBot() class because they're
    technically required in there only.
    
    Because the docs for the irc module are lacking, I've been poking around in
    the objects' respective directories and taking notes.
    
    Rewrote DixieBot.__init__() slightly.
    
    The callback methods look for events, not exceptions.  The prototypes for
    irc.bot.SingleServerIRCBot() look like this:
    
        def on_something(self, connection, event):
    
    Added callbacks on_privmsg() and on_pubmsg(), which does most of the heavy
    lifting for the bot's intended purpose.
    
    Added proper logging.
    
    Chopped out the last of the legacy code.

--- irc_bot/dixie_flatline_bot.py
+++ irc_bot/dixie_flatline_bot.py
@@ -3,9 +3,10 @@
 # vim: set expandtab tabstop=4 shiftwidth=4 :
 
 # This is my first serious attempt at writing an IRC bot in Python.  It uses
-#   implements some of the protocol natively, and uses MegaHAL as its
+#   the Python module called irc (https://pythonhosted.org/irc/), which
+#   implements the IRC protocol natively, and also uses MegaHAL as its
 #   conversation engine.  Don't expect this to be anything major, I'm just
-#   playing around.
+#   playing around to find good and bad ways of doing this.
 
 # By: The Doctor <drwho at virtadpt dot net>
 
@@ -14,9 +15,12 @@
 # v1.0 - Initial release.
 
 # TO-DO:
-# - Use a real logger.
+# - SSL/TLS support.
 
 # Load modules.
+# Needed because we're doing floating point division in a few places.
+from __future__ import division
+
 from megahal import *
 
 import argparse
@@ -49,13 +53,6 @@ owner = ""
 # How many backward chain links to run text through.  Defaults to three.
 order = 3
 
-# Criteria for which the bot will learn from its owner:
-# - Four or more letters
-# - Three or more words
-# - No lone numbers
-min_letters_per_word = 4
-min_words_per_line = 3
-
 # The location of the database the Markov model data is kept in.  This defaults
 # to ./.pymegahal-brain, per the MegaHAL python module's default.
 brainfile = "./.pymegahal-brain"
@@ -74,29 +71,145 @@ loglevel = ""
 # This is an instance of irc.bot which connects to an IRC server and channel
 #   and shadows its owner.
 class DixieBot(irc.bot.SingleServerIRCBot):
-    channel = ""
+    # Class-level constants which form static attributes.
+    # Criteria for which the bot will learn from its owner:
+    # - Four or more letters
+    # - Three or more words
+    # - No lone numbers
+    min_words_per_line = 3
+    min_letters_per_word = 4
 
-    def __init__(self, channel, nickname, server, port):
+    # Class-level variables which form attributes.
+    channel = ""
+    nick = ""
+    owner = ""
+
+    # Reference to the Markov brain.
+    brain = ""
+
+    # Methods on the connection object to investigate:
+    # connect() - Connect to a server?
+    # connected() - 
+    # disconnect() -
+    # get_nickname() - 
+    # get_server_name() - 
+    # info() - 
+    # ircname() - 
+    # is_connected() - See if the connection is still up?
+    # part() - Leave channel?
+    # privmsg() - Send privmsg?
+    # quit() - Terminate IRC connection?
+    # reconnect() - Reconnect to server?
+    # send_raw() - 
+    # stats() - 
+    # time() - 
+
+    def __init__(self, channel, nickname, server, port, nick, owner, brain):
+        # Initialize the class' attributes.
+        self.channel = channel
+        self.nick = nick
+        self.owner = owner
+        self.brain = brain
+
         # Initialize an instance of this class by running the parent class'
         # Default initializer method.
         # [(server, port)] can be a list of one or more (server, port) tuples
         # because it can connect to more than one at once.
         # The other two arguments are the bot's nickname and realname.
-        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname,
-            nickname)
-        self.channel = channel
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nick, nick)
 
     # This method fires if the configured nickname is already in use.  If that
     # happens, change the bot's nick slightly.
     # Note that the name of this method is specifically what the irc module
     # looks for.
-    def on_nicknameinuse(self, connection, exception):
+    def on_nicknameinuse(self, connection, event):
         connection.nick(connection.get_nickname() + "_")
 
     # This method fires when the server accepts the bot's connection.  It joins
     # the configured channel.
-    def on_welcome(self, connection, exception):
+    def on_welcome(self, connection, event):
         connection.join(self.channel)
+
+    # This method would fire when the bot receives a private message.  I don't
+    # have anything for this yet so it's a no-op.
+    def on_privmsg(self, connection, event):
+        pass
+
+    # This method fires every time a public message is posted to an IRC
+    # channel.  Technically, 'line' should be 'event' but I'm just now getting
+    # this module figured out...
+    def on_pubmsg(self, connection, line):
+
+        # IRC nick that sent a line to the channel.
+        sending_nick = line.source.split("!~")[0]
+
+        # See if the line fits the criteria for whether or not to learn from
+        # the line.  Start with whether or not there are enough words per
+        # line.
+        line = line.arguments[0]
+        words_in_line = 0
+        if line.split(':')[0] == self.nick:
+            words_in_line = len(line.split(' ')) - 1
+            words_in_line -= 1
+        else:
+            words_in_line = len(line.split(' '))
+        logger.debug("Words in line: " + str(words_in_line))
+
+        # If the line doesn't have enough words, skip it.
+        if words_in_line < self.min_words_per_line:
+            logger.debug("Line isn't long enough.")
+            return
+
+        # Count the number of letters in each word of the sentence.  If at
+        # least 75% are longer than four letters, learn from the sentence.
+        long_words = 0
+        for word in line.split(' '):
+            # Pre-emptively clean up the word a little.
+            word = word.strip(',')
+            word = word.strip('.')
+            word = word.strip(';')
+            word = word.strip()
+
+            if len(word) >= self.min_letters_per_word:
+                long_words += 1
+        logger.debug("Number of long words found in line: " + str(long_words))
+
+        # If the ratio of long words to short words in the line is less than
+        # 3/5, skip it.
+        if (long_words / words_in_line) < 0.6:
+            logger.debug("Line wasn't long enough to learn from.")
+            return
+
+        # If the line is from the bot's owner, learn from it and then decide
+        # whether to respond or not.
+        if sending_nick == self.owner:
+            logger.debug("Learning from text from the bot's owner.")
+            self.brain.learn(line)
+            self.brain.sync()
+            roll = random.randint(1, 10)
+            if roll == 1:
+                logger.debug("Posting a response to the channel.")
+                reply = self.brain.get_reply_nolearn(line)
+
+                # connection.privmsg() can be used to send text to either a
+                # channel or a user.
+                connection.privmsg(self.channel, reply)
+            return
+
+        # If the line is not from the bot's owner, decide randomly if the bot
+        # should learn from it, or learn from and respond to it.
+        roll = random.randint(1, 10)
+        if roll == 1:
+            logger.debug("Learning from the last line seen in the channel.")
+            self.brain.learn(line)
+            self.brain.sync()
+            return
+        if roll == 2:
+            logger.debug("Learning from the last line seen in the channel and responding to it.")
+            reply = self.brain.get_reply(line)
+            self.brain.sync()
+            connection.privmsg(channel, reply)
+            return
 
 # Functions.
 # Figure out what to set the logging level to.  There isn't a straightforward
@@ -186,11 +299,11 @@ if os.path.exists(config_file):
     brain = config.get("DEFAULT", "brain")
     loglevel = config.get("DEFAULT", "loglevel").lower()
 else:
-    logging.error("Unable to open configuration file " + config_file + ".")
+    logger.error("Unable to open configuration file " + config_file + ".")
 
 # IRC server to connect to.
 if not args.server:
-    print "ERROR: You must specify the hostname or IP of an IRC server at a minimum."
+    logger.fatal("ERROR: You must specify the hostname or IP of an IRC server at a minimum.")
     sys.exit(1)
 else:
     irc_server = args.server
@@ -205,16 +318,16 @@ if args.nick:
 
 # Channel to connect to.
 if not args.channel:
-    print "ERROR: You must specify a channel to join."
+    logger.fatal("ERROR: You must specify a channel to join.")
     sys.exit(1)
 else:
-    channel = "#" + args.channel
+    channel = args.channel
 
 # Nick of the bot's owner to follow around.
 if args.owner:
     owner = args.owner
 else:
-    print "ERROR: You must specify the nick of the bot's owner."
+    logger.fatal("ERROR: You must specify the nick of the bot's owner.")
     sys.exit(1)
 
 # Order of the Markov chains to construct.
@@ -225,7 +338,7 @@ if args.order:
 if args.brain:
     brainfile = args.brain
     if not os.path.exists(brainfile):
-        print "WARNING: The brainfile you've specified (" + brainfile + ") does not exist."
+        logger.fatal("WARNING: The brainfile you've specified (" + brainfile + ") does not exist.")
         sys.exit(1)
 
 # If a training file is available, grab it.
@@ -235,15 +348,14 @@ if args.trainingfile:
 # Let's not let people re-train existing Markov brains.  Because I'm tired and
 # punchy.
 if args.brain and args.trainingfile:
-    print "WARNING: It's a bad idea to re-train an existing brainfile with a new corpus."
-    print "I'll figure out how to do that later."
+    logger.fatal("WARNING: It's a bad idea to re-train an existing brainfile with a new corpus.  I'll figure out how to do that later.")
     sys.exit(1)
 
 # If an existing brain was specified, try to open it.  Else, create a new one
 # using the training corpus.
 brain = MegaHAL(order=order, brainfile=brainfile)
 if training_file:
-    print "Training the bot's Markov brain... this could take a while..."
+    logger.info("Training the bot's Markov brain... this could take a while...")
     brain.train(training_file)
     print "Done!"
 
@@ -262,19 +374,8 @@ logger = logging.getLogger(__name__)
 random.seed()
 
 # Instantiate a copy of the bot class.
-bot = DixieBot(channel, nickname, irc_server, irc_port)
-
-# MOOF MOOF MOOF - I know this is broken!
-# Roll 1d10.  On a 1, post a response to the channel.  On a 2, post a reply
-# and learn from it.
-roll = random.randint(1, 10)
-if roll == 1:
-    print "Responding to text without adding to the Markov brain."
-    sendmsg(ircsock, channel, brain.get_reply_nolearn(ircmsg))
-if roll == 2:
-    print "Responding to the text."
-    sendmsg(ircsock, channel, brain.get_reply(ircmsg))
-    brain.sync()
+bot = DixieBot(channel, nick, irc_server, irc_port, nick, owner, brain)
+bot.start()
 
 # Fin.
 brain.sync()
