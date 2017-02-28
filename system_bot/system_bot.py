10e8f3e2e380259e5af7e8fd36f1d1a7e3794584
The Doctor <drwho@virtadpt.net>
2017-02-27 22:50:01 -0800

    Started sketching out functions that acquire and return system status information.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- system_bot/system_bot.py
+++ system_bot/system_bot.py
@@ -18,15 +18,16 @@
 # - Put the full paths into /proc for the various system state variables into
 #   the configuration file, and not hardcoded.
 # - Make it possible to monitor specific processes running on the system, and
-#   potentially manipulate them with commands sent by the bot's owner.
+#   potentially manipulate them with commands sent by the bot's owner.  Use
+#   the method psutil.test() to dump the process list and pick through it
+#   looking for the stuff in question.
 
 # Load modules.
 import os
+import psutil
 import sys
 
 # Constants.
-# Full path to the system load.
-loadavg = "/proc/loadavg"
 
 # Full path to memory utilization stats.
 meminfo = "/proc/meminfo"
@@ -51,6 +52,51 @@ diskstats = "/proc/diskstats"
 # Classes.
 
 # Functions.
+# sysload(): Function that takes a snapshot of the current system load averages
+#   and returns them as a hash table.  Takes no arguments.
+def sysload():
+    sysload = {}
+    system_load = os.getloadavg()
+    sysload['one_minute'] = system_load[0]
+    sysload['five_minute'] = system_load[1]
+    sysload['fifteen_minute'] = system_load[2]
+    return sysload
+
+# uname(): Function that calls os.uname(), extracts a few things, and returns
+#   them as a hash table.  This should only be called upon request by the user,
+#   or maybe when the bot starts up.  There's no sense in having it run every
+#   time it loops because it changes so little.  Takes no arguments.
+def uname():
+    system_info = {}
+    sysinfo = os.uname()
+    system_info['hostname'] = sysinfo[1]
+    system_info['version'] = sysinfo[2]
+    system_info['buildinfo'] = sysinfo[3]
+    system_info['arch'] = sysinfo[4]
+    return system_info
+
+# cpus(): Takes no arguments.  Returns the number of CPUs on the system.
+def cpus():
+    return psutil.cpu_count()
+
+# cpu_idle_time(): Takes no arguments.  Returns the percentage of runtime the
+#CPUs are idle as a floating point number.
+def cpu_idle_time():
+    return psutil.cpu_times_percent()[3]
+
+# disk_usage(): Takes no arguments.  Returns a hash table containing the disk
+#   device name as the key and percentage used as the value.
+def disk_usage():
+    disk_usage = {}
+
+    disk_partitions = psutil.disk_partitions()
+    for i in disk_partitions:
+        disk_usage[i[0]] = ""
+
+# memory_utilization(): Function that returns the amount of memory free as a
+#   floating point value (a percentage).  Takes no arguments.
+def memory_utilization():
+    return psutil.virtual_memory().percent
 
 # Core code...
 
