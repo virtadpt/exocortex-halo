ad656a4a2a2870eb0377cc851de03d2f448eb962
The Doctor <drwho@virtadpt.net>
2017-11-11 22:00:56 -0800

    Started reworking it to use JSON externally, because that seems the lingua franca of APIs and it's easier to send and expect JSON than it is to send JSON but expect something else.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- hmac_a_tron/hmac_a_tron.py
+++ hmac_a_tron/hmac_a_tron.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 # vim: set expandtab tabstop=4 shiftwidth=4 :
 
-# hmac_a_tron.py - A microservice that takes a string of data POSTed to it
-#   along with an arbitrary string of some kind (read: secret key) and runs an
+# hmac_a_tron.py - A microservice that takes a JSON document POSTed to it
+#   contianing an arbitrary string of some kind (read: secret key) and runs an
 #   HMAC (https://en.wikipedia.org/wiki/Hash-based_message_authentication_code)
-#   on it.  The HMAC'd data is then returned to the client.
+#   on it.  The HMAC'd data is then returned to the client as a JSON document
+#   of the form { "result": "<HMAC here>" }.
 #
 #   The use case for this should be pretty obvious: You want to interact with
 #   an API programmatically but it requires that your requests be HMAC'd for
@@ -29,6 +30,7 @@ from BaseHTTPServer import BaseHTTPRequestHandler
 import argparse
 import hashlib
 import hmac
+import json
 import logging
 import sys
 
@@ -51,7 +53,10 @@ api_server = None
 class RESTRequestHandler(BaseHTTPRequestHandler):
 
     # Constants that make a few things easier later on.
-    required_parameters = [ "data", "secret" ]
+    required_keys = [ "data", "secret" ]
+
+    # Supported hash functions for the HMAC.
+    supported_hashes = [ "md5", "sha1", "sha224", "sha256", "sha384", "sha512" ]
 
     # Process HTTP/1.1 GET requests.
     def do_GET(self):
@@ -71,13 +76,22 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
         # Documentation.
         documentation = """
         <body>
-        <p>This is a microservice that accepts POST requests with two parameters:</p>
+        <p>This is a microservice that accepts POST requests that have JSON payloads with two keys in them:</p>
+
         <p>
         <ul>
         <li>data - A string of data to be HMAC'd.  All relevant data must be concatenated prior to sending it.</li>
         <li>secret - The secret HMAC key.</li>
         </ul>
         </p>
+
+        <p>For example:</p>
+        <p><code>
+        {
+            "data": "foo bar baz quux",
+            "secret": "12345"
+        }
+        </code></p>
 
         <p>The name of the HMAC algorithm is used as the URI.</p>
 
@@ -95,7 +109,13 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
 
         <p>For example: <b>http://localhost:10000/sha256</b></p>
 
-        <p>The final result will be returned as a string, e.g. <b>Langih3qui3uguo7GaJongaichiethahmi1g</b></p>
+        <p>The final result will be returned as a JSON document with a single key, <b>result</b>, like so:</p>
+        
+        <p><code>
+        {
+            "result": "Langih3qui3uguo7GaJongaichiethahmi1g"
+        }
+        </code></p>
 
         </body>
         """
@@ -103,6 +123,7 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
 
         # Bottom of the page.
         bottom_of_page = """
+        <br/><br/>
         <footer></footer>
         </html>
         """
@@ -114,7 +135,55 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         logger.debug("Entered RESTRequestHandler.do_POST().")
 
-        pass
+        hash = ""
+        content = ""
+        content_length = 0
+
+        # Parse the URI to see if it's one of the supported hashes.
+        logger.debug("URI requested by the client: " + str(self.path))
+        hash = self.path.strip("/")
+        if hash not in self.supported_hashes:
+            logger.debug("The user tried to use a non-existent hash: " + str(hash))
+            self._send_http_response(404, "That hash is unsupported.")
+            return
+
+        # Read any content sent from the client.  If there is no
+        # "Content-Length" header, something screwy is happening, in which
+        # case we fire an error.        
+        content = self._read_content()
+        if not content:
+            logger.debug("Client sent zero-length content.")
+            self._send_http_response(400, "You sent no content in that request.")
+            return
+
+        return
+
+    # Send an HTTP response, consisting of the status code, headers and
+    # payload.  Takes two arguments, the HTTP status code and a JSON document
+    # containing an appropriate response.
+    def _send_http_response(self, code, text):
+        self.send_response(code)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(text)
+        return
+
+    # Read content from the client connection and return it as a string.
+    # Return None if there isn't any content.
+    def _read_content(self):
+        content = ""
+        content_length = 0
+
+        try:
+            content_length = int(self.headers['Content-Length'])
+            content = self.rfile.read(content_length)
+            logger.debug("Content sent by client: " + content)
+        except:
+            logger.debug('{"result": null, "error": "Client sent zero-lenth content.", "id": 500}')
+            self._send_http_response(500, '{"result": null, "error": "Client sent zero-lenth content.", "id": 500}')
+            return None
+
+        return content    
 
 # Functions.
 # Figure out what to set the logging level to.  There isn't a straightforward
