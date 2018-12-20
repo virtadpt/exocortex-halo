b0bef74eb748eea74a7888f57771be51ad3e9097
The Doctor <drwho@virtadpt.net>
2018-12-19 21:14:44 -0800

    Rewrote the run.sh scripts to be more succinct and supervisord friendly.
    Pulled an unnecessary timeout from Systembot's supervisord config file.
    
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- system_bot/run.sh
+++ system_bot/run.sh
@@ -1,26 +1,10 @@
 #!/usr/bin/env bash
 
-# run.sh - Shell script that wraps around system_bot.py if you had
-#    to install it to a virtualenv like I did.  This script assumes that you
-#    set up the virtualenv the way you're supposed to and didn't tinker with
-#    the sandbox too much.  If you did, you're on your own.
+export VIRTUAL_ENV="$(pwd)/env"
+export PATH=$VIRTUAL_ENV/bin:$PATH
+export PS1="(virtualenv) $PS1"
+unset PYTHON_HOME
 
-# by: The Doctor [412/724/301/703/415][ZS] <drwho at virtadpt dot net>
+exec "./system_bot.py"
 
-# Core code.
-
-# Bring in the virtualenv.  Imagine whatever sci-fi sound effects you wish
-# as this happens.
-source env/bin/activate
-
-# Grab the first command line argument (which should be a number) and sleep
-# that many seconds.  This is totally a yucky hack.
-sleep $1
-shift
-
-# Call the bot with the remaining command line args.
-python ./system_bot.py $@
-
-# Clean up.
 exit 0
-
