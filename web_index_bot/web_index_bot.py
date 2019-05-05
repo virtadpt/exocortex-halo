e1e4e58c1f86723871479040c2e9722999ef40d9
The Doctor <drwho@virtadpt.net>
2019-05-05 11:00:15 -0700

    Fixed a dumb typo.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- web_index_bot/web_index_bot.py
+++ web_index_bot/web_index_bot.py
@@ -338,7 +338,7 @@ for i in search_engines:
 if user_text:
     logger.debug("User-defined help text: " + user_text)
 if user_acknowledged:
-    logger.debug("User-defined command acknowledgenent text: " + user_acknowledged)
+    logger.debug("User-defined command acknowledgement text: " + user_acknowledged)
 
 # Go into a loop in which the bot polls the configured message queue with each
 # of its configured names to see if it has any search requests waiting for it.
