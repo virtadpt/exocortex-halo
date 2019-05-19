72d444d177515d9baee3c19b2cf582d6a87418e3
The Doctor <drwho@virtadpt.net>
2019-05-19 14:54:21 -0700

    Cleaned up the setup code.  Deleted some boilerplate that's useless.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- command_line_messager/send_message.py
+++ command_line_messager/send_message.py
@@ -11,6 +11,10 @@
 
 # License: GPLv3
 
+# v2.1 - Reformatted many of the references to use double-quotes, like the rest
+#   of my stuff.
+#       - Changed some print()s to logging.fatal()s.
+#       - Changed logging.warn() to logging.warning(), per deprecation.
 # v2.0 - Ported to Python 3.
 # v1.0 - Initial release.
 
@@ -22,8 +26,6 @@ import json
 import logging
 import requests
 import sys
-
-# Constants.
 
 # Global variables.
 # Handle to an argument parser.
@@ -46,8 +48,6 @@ message = {}
 
 # Handle to a requests object.
 request = None
-
-# Classes.
 
 # Functions.
 # Figure out what to set the logging level to.  There isn't a straightforward
@@ -74,26 +74,26 @@ def process_loglevel(loglevel):
 argparser = argparse.ArgumentParser(description="A command line utility which allows the user to send text or data to an instance of the Exocortex XMPP Bridge.  An ideal use case for this tool is to make interactive jobs communicate with the rest of an exocortex.", epilog="If you want to redirect stdout or stderr from something else so this utility can transmit it, make the last argument a - (per UNIX convention) to catch them, like this:\n echo foo | %(prog)s -")
 
 # Set up the hostname of the XMPP bridge to contact.
-argparser.add_argument('--hostname', action='store', default='localhost',
+argparser.add_argument("--hostname", action="store", default="localhost",
     help="Specify the hostname of an XMPP bridge to contact.  Defaults to localhost.")
 
 # Set up the port of the XMPP bridge to contact.
-argparser.add_argument('--port', action='store', default=8003,
+argparser.add_argument("--port", action="store", default=8003,
     help="Specify the network port of an XMPP bridge to contact.  Defaults to 8003/tcp.")
 
 # Define the name of a message queue to send messages to.
-argparser.add_argument('--queue', action='store', default='replies',
+argparser.add_argument("--queue", action="store", default="replies",
     help="Specify a message queue of an XMPP bridge to contact.  Defaults to /replies.")
 
 # Loglevels: critical, error, warning, info, debug, notset.
-argparser.add_argument('--loglevel', action='store', default='info',
-    help='Valid log levels: critical, error, warning, info, debug, notset.  Defaults to INFO.')
+argparser.add_argument("--loglevel", action="store", default="info",
+    help="Valid log levels: critical, error, warning, info, debug, notset.  Defaults to INFO.")
 
 # Message to send to the XMPP bridge.
-argparser.add_argument('--message', action='store', nargs='*',
-    help='Text message to send to the XMPP bridge.')
-argparser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
-    help='Text stream to send to the XMPP bridge.  Give a - to use stdin.')
+argparser.add_argument("--message", action="store", nargs="*",
+    help="Text message to send to the XMPP bridge.")
+argparser.add_argument("infile", nargs="?", type=argparse.FileType("r"),
+    help="Text stream to send to the XMPP bridge.  Give a - to use stdin.")
 
 # Parse the command line args.
 args = argparser.parse_args()
@@ -105,27 +105,25 @@ logging.basicConfig(level=loglevel, format="%(levelname)s: %(message)s")
 logger = logging.getLogger(__name__)
 
 if not args.message and not args.infile:
-    print("ERROR: You need to specify either a message on the command line or a - to refer")
-    print("to the utility's stdin.")
-    sys.exit(1)
+    logging.fatal("You need to specify either a message on the command line or a - to refer to the utility's stdin.")
 
 logger.debug("Command line arguments presented to the script:")
 logger.debug(str(args))
 
 # Assemble the URL of the XMPP bridge to contact.
-message_queue = "http://" + args.hostname + ":" + str(args.port) + "/" + args.queue.strip('/')
+message_queue = "http://" + args.hostname + ":" + str(args.port) + "/" + args.queue.strip("/")
 
 # Set up custom headers.
-headers = {'Content-type': 'application/json'}
+headers = {"Content-type": "application/json"}
 
 # Build the message to send.
-message['name'] = args.queue.strip('/')
+message["name"] = args.queue.strip("/")
 if args.message:
-    message['reply'] = " ".join(word for word in args.message)
+    message["reply"] = " ".join(word for word in args.message)
 if args.infile:
     # Yeah, this is a little hacky, but it's clear enough to maintain later.
-    message['reply'] = args.infile.readlines()
-    message['reply'] = " ".join(word for word in message['reply'])
+    message["reply"] = args.infile.readlines()
+    message["reply"] = " ".join(word for word in message["reply"])
 
 # Attempt to contact the message queue and send a message.
 try:
@@ -134,7 +132,7 @@ try:
         data=json.dumps(message))
     logger.debug("Response from server: " + request.text)
 except:
-    logger.warn("Connection attempt to message queue failed.")
+    logger.warning("Connection attempt to message queue failed.")
 
 # Fin.
 sys.exit(0)
