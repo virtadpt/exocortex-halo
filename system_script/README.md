2097cdf5e9b25a55dfc8fe760de622875d52780a
The Doctor <drwho@virtadpt.net>
2021-03-20 15:05:30 -0700

    Initial commit of README.md file.
    Initial commit of the system-script.sh prototype.
    
    Signed-off-by: The Doctor <drwho@virtadpt.net>

---
+++ system_script/README.md
@@ -0,0 +1,41 @@
+System Script is a bot (maybe bot-adjacent?) which implements system monitoring in as close a fashion as [Systembot](/system_bot) but without all of the dependencies.  The use case here is devices running [OpenWRT](https://openwrt.org/) which don't have the storage capacity for a Python install like the one Systembot needs.
+
+It was designed specifically around [Busybox's](https://busybox.net/about.html) built in implementation of [ash](https://linux.die.net/man/1/ash), which is the default for just about every OpenWRT install out there.  This script only depends upon the [bc](https://www.gnu.org/software/bc/manual/html_mono/bc.html) command-line calculator utility, which can be installed with [the usual methods](https://openwrt.org/packages/start) in OpenWRT.  If it's not present System Script will error out.  This is due to the fact that pretty much every shell out there doesn't handle floating point values of any kind, but `bc` does, and can even do basic logic with them.
+
+At present, System Script is not interactive.  I want to get basic monitoring functionality working first before I try to make the utility more complex.
+
+System Script does not have any built-in way of getting alerts off-system.  It is designed to operate in the context of a separate messaging mechanism, be it a [cross-compiled](https://drwho.virtadpt.net/archive/2021-03-01/cross-compiling-go-sendxmpp/) copy of [go-sendxmpp](https://salsa.debian.org/mdosch/go-sendxmpp), a REST API like Prosody's [mod_rest](https://modules.prosody.im/mod_rest.html) with [cURL](https://curl.se/), Busybox's wget, [xmppcd](https://github.com/stanson-ch/xmppcd), or whatever other clever thing you might devise.  Please let me know what you come up with so I can add it to this file.
+
+The to-do list is pretty long, so don't expect to get full functionality just yet.  Implemented at this time:
+
+* Library functions
+    * Calculation of the average of a list of numbers
+    * Calculation of the standard deviation of a list of numbers
+* Monitoring of the one, five, and ten minute load averages
+* Monitoring of available space on all writable file systems mounted on the device
+
+On the roadmap:
+
+* Monitoring the memory usage of the device
+* Monitoring the temperature sensors in the device
+* Config file support
+* Command line options
+* Interactive commands
+* Alert cooldown so you don't get flooded with warnings every __n__ seconds
+
+Default settings:
+
+* Default number of samples averaged for each system metric: 10
+* Time to sleep in between loops: 10 seconds
+* Number of [standard deviations](https://www.mathsisfun.com/data/standard-deviation.html) which will trigger an alert: 2 sigma
+* Disk space usage danger zone: 85% of capacity
+
+Installation
+
+* Install the messaging utility of your choice.
+* Install bc.
+* Copy system-script.sh over to the system.
+* `./system-script.sh | your-messaging-utility`
+    * Set up automatic start at boot-time.
+    * Set up "make sure this script stays up" measures if you need to.
+
---
+++ system_script/system-script.sh
@@ -0,0 +1,316 @@
+#!/usr/bin/env -S busybox sh
+
+# system-script.sh - An experimental re-implementation of Systembot as a shell
+#   script, suitable for deployment on embedded devices running OpenWRT,
+#   and probably any Linux running ash or dash.  It outputs data to stdout
+#   and errors to stderr.  You're on your own for getting the output someplace
+#   to react to.
+#
+#   Accessory functions are as close to the functions that use them as
+#   feasible.
+#
+#   This script requires the presence of the bc utility to do math.  If it's
+#   not there it'll ABEND.
+
+# by: The Doctor [412/724/301/703/415/510] <drwho at virtadpt dot net>
+# License: GPLv3
+
+# This is part of the Exocortex Halo project
+#   (https://github.com/virtadpt/exocortex-halo).
+
+# PLEASE READ THE README.MD FILE BEFORE ATTEMPTING TO USE THIS UTILITY.  THIS
+# IS STILL A WORK IN PROGRESS.
+
+# v0.2 - Prototype release.
+
+# TODO:
+#   * Accept commands from outside.  Parse them and react.
+#   * Online help (--help / -h)
+#   * Debug mode (--debug / -d)
+#   * Move the list length calculations into a separate function.
+#   * Loadable (sourceable?) config file.
+#   * Memory usage
+#   * System temperature
+#   * Add a configurable cooldown period for each metric (i.e., only alert on
+#     dangerous storage usage every n seconds/hours/whatever).  This might
+#     take a little math.
+
+# Debug mode?
+DEBUG=0
+
+# Maximum number of elements in each "array."
+max_array_len=10
+
+# Time in seconds to sleep in between loops.
+cycle_time=10
+
+# Number of standard deviations to worry about.
+stddev=2
+
+# Arrays for the statistical analysis of device metrics.
+one_minute_load_history=""
+five_minute_load_history=""
+ten_minute_load_history=""
+
+# Standard deviations.
+one_minute_stddev=0
+five_minute_stddev=0
+ten_minute_stddev=0
+
+# Percentage of disk space used which is considered too high.
+disk_space_danger_zone=85
+
+# Functions
+# Library function which takes a list of numbers and calulates the average.
+calculate_list_average () {
+    # $1: Variable that holds the returned value.
+    # $2: List of numbers to average.
+    # $3: Length of the list of numbers.
+
+    local result=$1
+    local list_of_figures=$2
+    local list_length=$3
+
+    local average=0
+    
+    for i in $list_of_figures; do
+        average=$( echo "$average + $i" | bc -l )
+    done
+    average=$( echo "scale=2; $average/$list_length" | bc -l )
+    eval $result="'$average'"
+}
+
+# Library function which calculates the standard deviation
+#   (https://en.wikipedia.org/wiki/Standard_deviation) of the numbers in a
+#   list.
+calculate_list_stddev () {
+    # $1: Variable that holds the returned value.
+    # $2: List of numbers to average.
+    # $3: Length of the list of numbers.
+    # $4: Average of the list of numbers.
+
+    local result=$1
+    local list_of_figures=$2
+    local list_length=$3
+    local average=$4
+
+    # Local list which holds the deviations of the elements of the data set.
+    local population=""
+
+    # Length of the population list.
+    local population_length=0
+
+    # Variance of the population.
+    local variance=0
+
+    # Standard deviation of the population.
+    local stddev=0
+
+    # Calculate the deviation of each data point.
+    for i in $list_of_figures; do
+        deviation=$( echo "scale=2; ($i - $average) ^ 2" | bc -l )
+        if [ $population_length -eq 0 ]; then
+            population=$deviation
+        else
+            population="$population $deviation"
+        fi
+        population_length=$(( $population_length+1 ))
+    done
+
+    # Calculate the variance of the population.
+    calculate_list_average variance "$population" $population_length
+
+    # Calculate the standard deviation.
+    stddev=$( echo "scale=2; sqrt($variance)" | bc -l )
+    eval $result="'$stddev'"
+}
+
+# Primary function which gets and analyzes the system load on every loop.
+analyze_system_load () {
+    local one_minute_load_length=0
+    local five_minute_load_length=0
+    local ten_minute_load_length=0
+
+    local one_minute_load_average=0
+    local five_minute_load_average=0
+    local ten_minute_load_average=0
+
+    # Get system load.
+    load=$(top -n 1 | grep '^Load average')
+    one_minute_load=$(echo $load | awk '{print $3}')
+    five_minute_load=$(echo $load | awk '{print $4}')
+    ten_minute_load=$(echo $load | awk '{print $5}')
+
+    # Calculate the lengths of the lists.
+    for i in $one_minute_load_history; do
+        one_minute_load_length=$(( $one_minute_load_length+1 ))
+    done
+    for i in $five_minute_load_history; do
+        five_minute_load_length=$(( $five_minute_load_length+1 ))
+    done
+    for i in $ten_minute_load_history; do
+        ten_minute_load_length=$(( $ten_minute_load_length+1 ))
+    done
+
+    # Append the new readings to the lists.
+    if [ $one_minute_load_length -eq 0 ]; then
+        one_minute_load_history=$one_minute_load
+    else
+        one_minute_load_history="$one_minute_load_history $one_minute_load"
+    fi
+    one_minute_load_length=$(( $one_minute_load_length+1 ))
+
+    if [ $five_minute_load_length -eq 0 ]; then
+        five_minute_load_history=$five_minute_load
+    else
+        five_minute_load_history="$five_minute_load_history $five_minute_load"
+    fi
+    five_minute_load_length=$(( $five_minute_load_length+1 ))
+
+    if [ $ten_minute_load_length -eq 0 ]; then
+        ten_minute_load_history=$ten_minute_load
+    else
+        ten_minute_load_history="$ten_minute_load_history $ten_minute_load"
+    fi
+    ten_minute_load_length=$(( $ten_minute_load_length+1 ))
+
+    # Measure the length of each list and drop the oldest value if it's longer
+    # than the configured maximum.
+    if [ $one_minute_load_length -gt $max_array_len ]; then
+        one_minute_load_history=$(echo $one_minute_load_history | \
+            cut -d' ' -f2-)
+    fi
+    if [ $five_minute_load_length -gt $max_array_len ]; then
+        five_minute_load_history=$(echo $five_minute_load_history | \
+            cut -d' ' -f2-)
+    fi
+    if [ $ten_minute_load_length -gt $max_array_len ]; then
+        ten_minute_load_history=$(echo $ten_minute_load_history | \
+            cut -d' ' -f2-)
+    fi
+
+    # Calculate the load averages.
+    if [ $one_minute_load_length -gt 5 ]; then
+        calculate_list_average \
+            one_minute_load_average \
+            "$one_minute_load_history" \
+            $one_minute_load_length
+    fi
+
+    if [ $five_minute_load_length -gt 5 ]; then
+        calculate_list_average \
+            five_minute_load_average \
+            "$five_minute_load_history" \
+            $five_minute_load_length
+    fi
+
+    if [ $ten_minute_load_length -gt 5 ]; then
+        calculate_list_average \
+            ten_minute_load_average \
+            "$ten_minute_load_history" \
+            $ten_minute_load_length
+    fi
+
+    # Calculate the standard deviation of each list of values.
+    if [ $one_minute_load_length -gt 5 ]; then
+        calculate_list_stddev \
+            one_minute_stddev \
+            "$one_minute_load_history" \
+            $one_minute_load_length \
+            $one_minute_load_average
+
+        # Once again, shells not doing floating point math bites us in the
+        # ass.  Thankfully `bc` can help here, too.
+        if [ $( echo "$one_minute_stddev >= $stddev" | bc -l ) -ne 0 ]; then
+            echo "The system load has spiked to $one_minute_load on the one minute average."
+            echo "This is $one_minute_stddev standard deviations beyond what is safe."
+        fi
+    fi
+
+    if [ $five_minute_load_length -gt 5 ]; then
+        calculate_list_stddev \
+            five_minute_stddev \
+            "$five_minute_load_history" \
+            $five_minute_load_length \
+            $five_minute_load_average
+
+        if [ $( echo "$five_minute_stddev >= $stddev" | bc -l ) -ne 0 ]; then
+            echo "The system load has spiked to $five_minute_load on the five minute average."
+            echo "This is $five_minute_stddev standard deviations beyond what is safe."
+        fi
+    fi
+
+    if [ $ten_minute_load_length -gt 5 ]; then
+        calculate_list_stddev \
+            ten_minute_stddev \
+            "$ten_minute_load_history" \
+            $ten_minute_load_length \
+            $ten_minute_load_average
+
+        if [ $( echo "$ten_minute_stddev >= $stddev" | bc -l ) -ne 0 ]; then
+            echo "The system load has spiked to $ten_minute_load on the ten minute average."
+            echo "This is $ten_minute_stddev standard deviations beyond what is safe."
+        fi
+    fi
+}
+
+# Primary function which gets and analyzes storage space usage for non-in
+#   memory and non-read only file systems on each loop.
+analyze_storage_space () {
+
+    local filesystems=""
+
+    # Isolate the filesystems we care about.
+    filesystems=$( mount | egrep -v '(ro|sysfs|tmp|devpts|debugfs)' )
+    filesystems=$( echo $filesystems | awk '{print $3}' )
+
+    # Having /overlay around confuses things.  We can't actually get its disk
+    # space reading.  However, it's the same filesystem as /, so we're not
+    # actually losing anything.
+    filesystems=$( echo $filesystems | grep -v 'overlay' )
+
+    # Fuzz out the disk space used for every remaining filesystem.
+    for i in $filesystems; do
+        local fs=""
+        local usage=0
+
+        fs=$(df -h $i | tail -n1 | awk '{ print $6 }')
+        usage=$(df -h $i | tail -n1 | awk '{ print $5 }' | sed 's/%//')
+
+        # Is the filesystem in the danger zone?
+        if [ $( echo "$usage >= $disk_space_danger_zone" | bc -l ) -ne 0 ]; then
+            echo "Warning: Filesystem $fs is at $usage% of capacity."
+            echo "This is too close to the configured danger zone."
+            echo "Please investigate this system at your earliest convenience."
+        fi
+    done
+}
+
+# analyze_memory_usage () {
+# }
+
+# check_system_temperature () {
+# }
+
+# Setup the script environment.
+# See if bc is present.  If it's not, ABEND.
+which bc > /dev/null
+if [ $? -gt 0 ]; then
+    echo "ERROR: bc is not installed.  This utility won't work without it."
+    echo "       Install that package and try again."
+    exit 1
+fi
+
+# Main loop.
+while true; do
+    analyze_system_load
+    analyze_storage_space
+
+    echo "Sleeping..."
+    sleep $cycle_time
+    echo
+done
+
+# Fin.
+exit 0
+
