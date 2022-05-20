1f8b77ac6e3ef8516569bbf651d1996eb1e7a18d
The Doctor <drwho@virtadpt.net>
2022-05-19 18:12:12 -0700

    Fixed a rounding bug that's been annoying me for a while.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- system_bot/system_bot.py
+++ system_bot/system_bot.py
@@ -547,7 +547,7 @@ while True:
                 # a GB / y GB (b%)  free.
                 message = message + system_stats.convert_bytes(info.free + info.buffers + info.cached)
                 message = message + " / " + system_stats.convert_bytes(info.total)
-                message = message + " (" + str(100.0 - info.percent) + "%) free."
+                message = message + " (" + str(round(100.0 - info.percent), 2) + "%) free."
                 logging.debug("Value of message: " + str(message))
                 send_message_to_user(message)
 
