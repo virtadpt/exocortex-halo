2bd93d19035b7a962122b0e1a031d8e91cf4e81e
The Doctor <drwho@virtadpt.net>
2023-03-14 19:41:08 -0700

    Turned this script into a module that can be loaded.  Also, first time I've ever tried to write a conditional main() that can test the other code.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- weather_station_bot/bme280_sensor.py
+++ weather_station_bot/bme280_sensor.py
@@ -15,15 +15,11 @@
 # TO-DO:
 # - Make the sensor's I2C address configurable.
 # - Use real logger code rather than a bunch of print()s.
-# - Turn this into a loadable module for the actual bot.
-# - Make the sleep time optional.  Eventually this code will be called as a
-#   module from the real bot.
 
 # Load modules.
 import bme280
 import smbus2
 import sys
-import time
 
 # Constants.
 port = 1
@@ -31,31 +27,28 @@ port = 1
 # I2C address of the sensor.
 address = 0x77
 
-# Global variables.
-# Handle to an smbus2 port.
-bus = None
-
-# Raw data from the sensor.
-bme280_data = None
-
-# If this is a class or module, say what it is and what it does.
-
-# Classes.
-
 # Functions.
+# get_reading() - Get a data sample from the sensor.  Data will not be
+#   rounded, converted, or anything else.  That needs to happen elsewhere.
+def get_reading():
+    # Handle to an SMbus object.
+    bus = None
 
-# Core code...
-# Get a handle to the SMbus.
-try:
-    bus = smbus2.SMBus(port)
-    bme280.load_calibration_params(bus, address)
-except:
-    print("Unable to get access to the SMbus.")
-    sys.exit(1)
+    # Handle to a sensor sample.
+    bme280_data = None
 
-# Research code: Pull data from the sensor every five seconds and print it to
-# stdout.
-while True:
+    # Hash table of data from the sensor sample.
+    data = {}
+
+    # Get a handle to the SMbus.
+    try:
+        print("Trying to get access to the sensor.")
+        bus = smbus2.SMBus(port)
+        bme280.load_calibration_params(bus, address)
+    except:
+        print("Unable to get access to the SMbus.")
+        return(None)
+
     # Structure of the data sample returned below:
     #   .id - A GUID.  String.
     #   .timestamp - ISO 8601 timestamp.  String.
@@ -63,14 +56,30 @@ while True:
     #   .pressure - Pressure in hPa/millibars.  Float.
     #       Standard pressure at sea level is 1013 hPa.
     #   .humidity - Relative humidity.  Float.
+    # Get a sample from the sensor.
     bme280_data = bme280.sample(bus, address)
-    print("Time: %s" % bme280_data.timestamp)
-    print("Temperature: %s degrees C" % round(bme280_data.temperature,
-        ndigits=2))
-    print("Pressure: %s kPa" % round(bme280_data.pressure, ndigits=2))
-    print("Humidity: %s%%" % round(bme280_data.humidity, ndigits=2))
 
-    print()
-    time.sleep(5)
+    # Populate the sample.
+    data["timestamp"] = bme280_data.timestamp
+    data["temperature"] = bme280_data.temperature
+    data["pressure"] = bme280_data.pressure
+    data["humidity"] = bme280_data.humidity
+
+    return(data)
+
+# Core code...
+# If we call this file by itself, exercise the do-stuff function.
+if __name__ == "__main__":
+    sample = get_reading()
+    if not sample:
+        print("Sensor did not return data.")
+        sys.exit(1)
+
+    print("Time: %s" % sample["timestamp"])
+    print("Temperature: %s degrees C" % round(sample["temperature"], ndigits=2))
+    print("Pressure: %s kPa" % round(sample["pressure"], ndigits=2))
+    print("Humidity: %s%%" % round(sample["humidity"], ndigits=2))
+
+    sys.exit(0)
 
 # Fin.
