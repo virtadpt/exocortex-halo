3a71be68bb1c7edee780e316fd25c8396e876929
The Doctor <drwho@virtadpt.net>
2023-04-09 16:14:54 -0700

    Added a runscript and systemd service file.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

---
+++ weather_station_bot/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export VIRTUAL_ENV="$(pwd)/env"
+export PATH=$VIRTUAL_ENV/bin:$PATH
+export PS1="(virtualenv) $PS1"
+unset PYTHON_HOME
+
+eval exec "python ./weather_station_bot.py"
+
+exit 0
